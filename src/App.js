e899ae3afa8fd8ee6981599f133d7eda501710e3
sagan <sagan@syneforge.com>
2020-08-06 21:25:43 +0300

    fix problem

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React, { useEffect } from 'react';
 import TodoList from "./Todo/TodoList";
-
 import Context from "./context";
 import Loader from "./Loader";
 import Modal from "./Modal/Modal";
 
-const AddTodo = React.lazy(() => import('./Todo/AddTodo'));
+const AddTodo = React.lazy(() => import('./Todo/AddTodo')
+);
 
 function App() {
   const [todos, setTodos] = React.useState([]);
   const [loading, setLoading] = React.useState(true);
 
   useEffect(() => {
-      fetch('https://jsonplaceholder.typicode.com/todos?_limit=10')
+      fetch('https://jsonplaceholder.typicode.com/todos?_limit=5')
           .then(response => response.json())
           .then(todos => {
               setTimeout(() => {
@@ -31,11 +31,11 @@ function App() {
               return todo
           })
       )
-  };
+  }
 
   function removeTodo(id) {
      setTodos(todos.filter(todo => todo.id !== id))
-  };
+  }
 
   function addTodo(title) {
      setTodos(todos.concat([
@@ -45,7 +45,7 @@ function App() {
            completed: false
        }
      ]))
-  };
+  }
 
   return (
     <Context.Provider value={{ removeTodo }}>
